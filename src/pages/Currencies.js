a989b64ddcde32939f014e57ea50c9b4132519c0
Norman <whosnorm@gmail.com>
2022-10-26 10:45:28 -0400

    first commit

---
+++ src/pages/Currencies.js
@@ -0,0 +1,9 @@
+const Currencies = () => {
+    return (
+        <div>
+            <h1>Currencies Page</h1>
+        </div>
+    )
+}
+
+export default Currencies
---
+++ src/pages/Price.js
@@ -0,0 +1,9 @@
+const Price = () => {
+    return (
+        <div>
+            <h1>Price Page</h1>
+        </div>
+    )
+}
+
+export default Price
